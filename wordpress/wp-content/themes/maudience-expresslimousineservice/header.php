0dd54909e389a8c2b98270e6411145812f97ac3a
Pete Lower <pete.lower@gmail.com>
2015-05-02 15:00:15 -0700

    add topnav styling and header layout

--- wordpress/wp-content/themes/maudience-expresslimousineservice/header.php
+++ wordpress/wp-content/themes/maudience-expresslimousineservice/header.php
@@ -36,28 +36,31 @@
 	<header id="masthead" class="site-header" role="banner">
 		<div id="inner-header-wrap" class="inner-header-wrap inner-wrap">
 			
-			<nav id="top-nav" class="top-nav">
-				<ul id="top-nav-menu" class="top-nav-menu menu">
-					<li id="email-menu-item" class="email-menu-item menu-item"><?php echo do_shortcode('[contactemail]'); ?></li>
-					<li id="phonenumber-menu-item" class="phonenumber-menu-item menu-item"><?php echo do_shortcode('[phonenumber]'); ?></li>
-				</ul>
-			</nav>
+			<div id='header-column-left' class='header-column-left header-column'>
+				<?php if ( get_header_image() ) : ?>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php header_image(); ?>" class="header-image" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" /></a>
+				<?php endif; ?>
+			</div>
 
-			<?php if ( get_header_image() ) : ?>
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php header_image(); ?>" class="header-image" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" /></a>
-			<?php endif; ?>
+			<div id='header-column-right' class='header-column-right header-column'>
+				<nav id="top-nav" class="top-nav">
+					<ul id="top-nav-menu" class="top-nav-menu menu">
+						<li id="email-menu-item" class="email-menu-item menu-item"><?php echo do_shortcode('[contactemail]'); ?></li>
+						<li id="phonenumber-menu-item" class="phonenumber-menu-item menu-item"><?php echo do_shortcode('[phonenumber]'); ?></li>
+					</ul>
+				</nav>
 
-			<hgroup>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-			</hgroup>
+				<hgroup>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+				</hgroup>
 
-			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<button class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></button>
-				<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
-				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
-			</nav><!-- #site-navigation -->
-
+				<nav id="site-navigation" class="main-navigation" role="navigation">
+					<button class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></button>
+					<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+				</nav><!-- #site-navigation -->
+			</div>
 			
 		</div><!-- #inner-header-wrap -->
 	</header><!-- #masthead -->
